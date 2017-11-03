1b254f6040241de7765e397506aff75fa76e3924
Brian Begy <brian.begy@guaranteedrate.com>
2017-11-03 08:57:08 -0500

    fix copyright

--- GuaranteedRate.Common.Configuration/Properties/AssemblyInfo.cs
+++ GuaranteedRate.Common.Configuration/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Guaranteed Rate")]
 [assembly: AssemblyProduct("GuaranteedRate.Common.Configuration")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © GuaranteedRate 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
