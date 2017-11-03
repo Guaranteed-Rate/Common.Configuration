d4cd1b06e109d6b52f2dcd7d3443dda7d9f3b8b0
Brian Begy <brian.begy@guaranteedrate.com>
2017-11-03 09:08:16 -0500

    very more better English.

--- GuaranteedRate.Common.Configuration/ConfigHelper.cs
+++ GuaranteedRate.Common.Configuration/ConfigHelper.cs
@@ -5,13 +5,13 @@ namespace GuaranteedRate.Common.Configuration
     public static class ConfigHelper
     {
         /// <summary>
-        /// attempts to get an app setting, returning a default if not found or not castable.
+        /// Attempts to get an app setting, returning a default if not found or not castable.
         /// </summary>
         /// <typeparam name="T">Type of the value you want</typeparam>
         /// <param name="key">Key Name e.g. "TimeoutValue" </param>
-        /// <param name="defaultValue">if not found or wrong type, what should we use?</param>
-        /// <param name="errorOnWrongType">blow up if it won't cast cleanly</param>
-        /// <returns></returns>
+        /// <param name="defaultValue">If not found or wrong type, what should we use?</param>
+        /// <param name="errorOnWrongType">Blow up if it won't cast cleanly.</param>
+        /// <returns>value of type T, either the value from app.config/web.config or defaultValue.</returns>
         public static T GetAppSetting<T>(string key, T defaultValue, bool errorOnWrongType = false)
         {
             var result = System.Configuration.ConfigurationManager.AppSettings.Get(key);
--- GuaranteedRate.Common.Configuration/ConfigWrapper.cs
+++ GuaranteedRate.Common.Configuration/ConfigWrapper.cs
@@ -8,13 +8,13 @@
     {
 
         /// <summary>
-        /// attempts to get an app setting, returning a default if not found or not castable.
+        /// Attempts to get an app setting, returning a default if not found or not castable.
         /// </summary>
         /// <typeparam name="T">Type of the value you want</typeparam>
         /// <param name="key">Key Name e.g. "TimeoutValue" </param>
-        /// <param name="defaultValue">if not found or wrong type, what should we use?</param>
-        /// <param name="errorOnWrongType">blow up if it won't cast cleanly</param>
-        /// <returns>T</returns>
+        /// <param name="defaultValue">If not found or wrong type, what should we use?</param>
+        /// <param name="errorOnWrongType">Blow up if it won't cast cleanly.</param>
+        /// <returns>value of type T, either the value from app.config/web.config or defaultValue.</returns>
         public T GetAppSetting<T>(string key, T defaultValue, bool errorOnWrongType = false)
         {
             return ConfigHelper.GetAppSetting<T>(key, defaultValue, errorOnWrongType);
